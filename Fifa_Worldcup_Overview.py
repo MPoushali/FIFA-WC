cb44cff16626c0390683c94ee597ab7ab366bd9e
Poushali <36698628+MPoushali@users.noreply.github.com>
2022-11-15 22:00:47 +0530

    Add files via upload

---
+++ Fifa_Worldcup_Overview.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+# st.header("FIFA Wordlcup Overview")
+
+st.markdown("""
+    # FIFA Wordlcup Overview
+    ### [Visit the FIFA Archive](https://www.fifa.com/fifaplus/en/archive)
+    """)
